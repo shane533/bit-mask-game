148f20ad8e71ca1e9de6285af4e39691666f7fb0
shane533 <ptez533@gmail.com>
2026-01-31 08:16:15 +0800

    update

--- operator_button.gd
+++ operator_button.gd
@@ -1,10 +1,28 @@
+@tool
 extends TextureButton
 class_name OperatorButton
 
 @export var label: Label
 
-var _op_type: int = 0 # 0 or ,1 and, 2 xor 
+@export var isFixed:bool = false
+@export var opType: int = 0:
+	set(op):
+		opType = op # 0 or ,1 and, 2 xor 
+		update_value()
 var _bit_flag: bool = true
+
+const operator_button_tscn = preload("res://operator_button.tscn")
+
+func init(pDefault:bool, pFixed:bool):
+	opType = pDefault
+	isFixed = pFixed
+	update_value()
+	
+static func new_operator_button(pDefault:bool, pFixed:bool) -> OperatorButton:
+	var ob:OperatorButton = operator_button_tscn.instantiate()
+	ob.init(pDefault, pFixed)
+	return ob
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,22 +34,27 @@ func _process(delta: float) -> void:
 	pass
 
 
+func update_value() -> void:
+	match opType:
+		0: # or
+			self.material.set_shader_parameter("mode", 1)
+			label.label_settings.font_color = Color.WHITE 
+			label.text = "OR"
+		1: #and
+			self.material.set_shader_parameter("mode", 0)
+			label.label_settings.font_color = Color.BLACK
+			label.text = "AND"
+		2: # xor
+			self.material.set_shader_parameter("mode", 2)
+			label.label_settings.font_color = Color.BLACK
+			label.text = "XOR"
+
 func change_value() -> void:
-	_op_type = (_op_type+1) % 3
-	print("OPchange_value %d " % _op_type)
-	if _op_type==0: # or
-		self.material.set_shader_parameter("mode", 1)
-		label.label_settings.font_color = Color.WHITE 
-		label.text = "OR"
-	elif _op_type==1: #and
-		self.material.set_shader_parameter("mode", 0)
-		label.label_settings.font_color = Color.BLACK
-		label.text = "AND"
-	elif _op_type==2: # xor
-		self.material.set_shader_parameter("mode", 2)
-		label.label_settings.font_color = Color.BLACK
-		label.text = "XOR"
+	opType = (opType+1) % 3
+	print("OPchange_value %d " % opType)
+	
 
 func _on_pressed() -> void:
-	change_value()
+	if not isFixed:
+		change_value()
 	pass # Replace with function body.
--- param_button.gd
+++ param_button.gd
@@ -1,25 +1,40 @@
+@tool
 extends TextureButton
 
 class_name ParamButton
 
 @export var label: Label
 
+@export var isFixed: bool = false
 
-var _bit_flag: bool = true
+@export var bitFlag: bool = true:
+	set(v):
+		#print("CALL SET")
+		bitFlag = v
+		update_value()
 
+const param_button_tscn = preload("res://param_button.tscn")
+
+func init(pDefault:bool, pFixed:bool):
+	bitFlag = pDefault
+	isFixed = pFixed
+	update_value()
+	
+static func new_param_button(pDefault:bool, pFixed:bool) -> ParamButton:
+	var pb:ParamButton = param_button_tscn.instantiate()
+	pb.init(pDefault, pFixed)
+	return pb
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func change_value() -> void:
-	_bit_flag = !_bit_flag
-	if _bit_flag:
+func update_value() -> void:
+	if bitFlag:
 		self.material.set_shader_parameter("mode", 1)
 		label.label_settings.font_color = Color.WHITE 
 		label.text = "1"
@@ -28,7 +43,11 @@ func change_value() -> void:
 		label.label_settings.font_color = Color.BLACK
 		label.text = "0"
 
+func change_value() -> void:
+	bitFlag = !bitFlag
+	update_value()
+
 func _on_pressed() -> void:
-	
-	change_value()
+	if not isFixed:
+		change_value()
 	pass # Replace with function body.
